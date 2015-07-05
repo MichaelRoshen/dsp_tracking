0512759e6d2d8f474c0dbf17528dda3619f090e0
root <root@vm10-254-170-80.ksc.com>
2015-07-05 23:36:45 +0800

    fluentd config

--- initializers/fluent.rb
+++ initializers/fluent.rb
@@ -1,4 +1,3 @@
 require 'fluent-logger'
-
-FLUENT = Fluent::Logger::FluentLogger.open(nil, :host=>'localhost', :port=>24224)
+FLUENT = Fluent::Logger::FluentLogger.open(nil, :host=>'10.254.212.149', :port=>24224)
 
