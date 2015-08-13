820a2caf7cbc0906be458edc29eaa93bf9de55f3
cc90146 <cc90146@alibaba-inc.com>
2015-08-13 13:11:34 +0800

    add bc & youku delivery price to redis

--- controllers/root.rb
+++ controllers/root.rb
@@ -6,12 +6,6 @@ end
 
 get '/api/ads' do
 	options = params.symbolize_keys
-	# if options[:hqSource].to_s == 'youku'
-	# 	wp = options[:hqPrice].to_s
-	# 	price = decode_aes_price(wp)
-	# 	options = options.merge({:decodePrice => price})
-	# elsif options[:hqSource].to_s == 'baidu_bc'
-	# end
 
 	if options[:hqEvent].to_s  == "1" #曝光
 		ShowTracking << options
@@ -31,35 +25,18 @@ get '/api/ads' do
 	end
 
 	if options[:hqEvent].to_s == '3' #竞价胜利
-	    rtb_key = "ad_#{params[:hqAdId]}_delivery_strategy"
-	    keys = ["id", "budget_price", "cycle_delivery_price", "ad_current_cost", "ad_cost", "delivery_time_limit", "delivery_price", "delivery_start_time", "delivery_end_time", "ad_group_budget_price", "ad_group_cost", "account_buget_price", "account_cost"]
+	    rtb_key = "ad_#{options[:hqAdId]}_delivery_strategy"
+	    keys = ["id", "budget_price", "cycle_delivery_price", "ad_current_cost", "ad_cost", "delivery_time_limit", "delivery_price_youku","delivery_price_bc", "delivery_start_time", "delivery_end_time", "ad_group_budget_price", "ad_group_cost", "account_buget_price", "account_cost"]
 		redis_info =  RtbRedis.expense_node.mapped_hmget(rtb_key, *keys)
 		redis_info = redis_info.symbolize_keys
-		tactual_price = options[:hqPrice]
-		tactual_price = tactual_price.to_f / 1000
-		#账号花费
-		redis_info[:account_cost] = (redis_info[:account_cost].to_f + tactual_price).round(2)
-		#广告组花费
-		redis_info[:ad_group_cost] = (redis_info[:ad_group_cost].to_f + tactual_price).round(2)
-		#广告当前周期花费
-		redis_info[:ad_current_cost] = (redis_info[:ad_current_cost].to_f + tactual_price).round(2)
-		#广告总花费
-		redis_info[:ad_cost] = (redis_info[:ad_cost].to_f + tactual_price).round(2)
+		delivery_price = get_greater_price(redis_info)
+		tactual_price = options[:hqPrice].to_f / 1000
+		update_ad_cost(redis_info, tactual_price)
 		#更新广告花费
 		RtbRedis.expense_node.mapped_hmset(rtb_key, redis_info)
-
-		#实际花费＋固定出价 超出15分钟的预算，则从可投放数组种删除该条广告，并存入不可投递数组种，等待定时任务重新计算
-		if (redis_info[:ad_current_cost].to_f + (redis_info[:delivery_price].to_f / 1000)) > redis_info[:cycle_delivery_price].to_f ||
-			Time.now + 15*60 > Time.at(redis_info[:delivery_end_time].to_i) 
-			#删除可投地数组中的id
-			budget_key = "budget_control"
-			RtbRedis.expense_node.srem(budget_key, params[:hqAdId].to_i)
-
-			#添加到不可投递数组中
-			no_budget_key = "no_budget_control"
-			RtbRedis.expense_node.sadd(no_budget_key, params[:hqAdId].to_i)
-		end
+		budget_control(redis_info, delivery_price, options)
 	end
+
 	des_url = options[:hqURL].to_s
 	if des_url != ""	
 	   redirect des_url, 302	
@@ -68,13 +45,35 @@ end
 
 private
 
-# def actual_price(options)
-# 	if options[:hqSource] == 'youku'
-# 		options[:decodePrice].to_i
-# 	else
-# 		options[:hqPrice].to_i
-# 	end
-# end
+def budget_control(redis_info, delivery_price, options)
+	#实际花费＋固定出价 超出15分钟的预算，则从可投放数组种删除该条广告，并存入不可投递数组种，等待定时任务重新计算
+	if (redis_info[:ad_current_cost].to_f + (delivery_price / 1000)) > redis_info[:cycle_delivery_price].to_f ||
+		Time.now + 15*60 > Time.at(redis_info[:delivery_end_time].to_i) 
+		#删除可投地数组中的id
+		RtbRedis.expense_node.srem("budget_control", options[:hqAdId].to_i)
+
+		#添加到不可投递数组中
+		RtbRedis.expense_node.sadd("no_budget_control", options[:hqAdId].to_i)
+	end
+end
+
+def get_greater_price(redis_info)
+	redis_info[:delivery_price_youku].to_f > redis_info[:delivery_price_bc].to_f ? redis_info[:delivery_price_youku].to_f : redis_info[:delivery_price_bc].to_f
+end
+
+def update_ad_cost(redis_info, price)
+	#账号花费
+	redis_info[:account_cost] = (redis_info[:account_cost].to_f + price).round(2)
+	#广告组花费
+	redis_info[:ad_group_cost] = (redis_info[:ad_group_cost].to_f + price).round(2)
+	#广告当前周期花费
+	redis_info[:ad_current_cost] = (redis_info[:ad_current_cost].to_f + price).round(2)
+	#广告总花费
+	redis_info[:ad_cost] = (redis_info[:ad_cost].to_f + price).round(2)
+
+	redis_info
+end
+
 
 
 def decode_aes_price(wp)
@@ -100,5 +99,5 @@ def decode_mega_price(wp)
 end
 
 def decode_gdt_price(wp)
-Rtb::Gdt::DecodePrice.decode_price(wp)
+	Rtb::Gdt::DecodePrice.decode_price(wp)
 end
--- lib/tasks/delivery_plan.rake
+++ lib/tasks/delivery_plan.rake
@@ -6,15 +6,15 @@
 
    desc 'reset :reset no_budget_control'
    task :reset do
-   	  require 'time'
-   	  no_budget_key = "no_budget_control"
+	   require 'time'
+	   no_budget_key = "no_budget_control"
       RtbRedis.expense_node.smembers(no_budget_key).uniq.map{|item|item.to_i}.each do |ad|
-      	puts ad
-      	rtb_key = "ad_#{ad.to_i}_delivery_strategy"
-	    keys = ["id", "budget_price", "cycle_delivery_price", "ad_current_cost", "ad_cost", "delivery_time_limit", "delivery_price", "delivery_start_time", "delivery_end_time", "ad_group_budget_price", "ad_group_cost", "account_buget_price", "account_cost"] 
-		redis_info =  RtbRedis.expense_node.mapped_hmget(rtb_key, *keys)
-		redis_info = redis_info.symbolize_keys
-		puts redis_info
+      puts ad
+      rtb_key = "ad_#{ad.to_i}_delivery_strategy"
+	   keys = ["id", "budget_price", "cycle_delivery_price", "ad_current_cost", "ad_cost", "delivery_time_limit", "delivery_price_youku","delivery_price_bc", "delivery_start_time", "delivery_end_time", "ad_group_budget_price", "ad_group_cost", "account_buget_price", "account_cost"]
+      redis_info =  RtbRedis.expense_node.mapped_hmget(rtb_key, *keys)
+   	redis_info = redis_info.symbolize_keys
+   	puts redis_info
       	if redis_info[:budget_price].to_f - redis_info[:ad_cost].to_f > redis_info[:cycle_delivery_price].to_f &&
       		redis_info[:ad_group_budget_price].to_f - redis_info[:ad_group_cost].to_f > redis_info[:cycle_delivery_price].to_f &&
       		redis_info[:account_buget_price].to_f - redis_info[:account_cost].to_f > redis_info[:cycle_delivery_price].to_f &&
@@ -23,20 +23,23 @@
       		puts "delivery agin...."
       		redis_info[:ad_current_cost] = 0.0
       		budget_key = "budget_control"
-			RtbRedis.expense_node.mapped_hmset(rtb_key, redis_info)
-			RtbRedis.expense_node.sadd(budget_key, redis_info[:id])
-			RtbRedis.expense_node.srem(no_budget_key, redis_info[:id])
+      		RtbRedis.expense_node.mapped_hmset(rtb_key, redis_info)
+      		RtbRedis.expense_node.sadd(budget_key, redis_info[:id])
+      		RtbRedis.expense_node.srem(no_budget_key, redis_info[:id])
       	end
       end
    end   
 
    desc 'redis seed'
    task :redis_seed do 
-	hash = {"id"=>"15", "budget_price"=>"120000", "cycle_delivery_price"=>"312", "ad_current_cost"=>"0.0", "ad_cost"=>"0.0", "delivery_time_limit"=>"96", "delivery_price"=>"600", "delivery_start_time"=>"1438790400", "delivery_end_time"=>"1439136000", "ad_group_budget_price"=>"120000", "ad_group_cost"=>"0.0", "account_buget_price"=>"200000", "account_cost"=>"0.0"}
-	rtb_key = "ad_15_delivery_strategy"
+   id = 13
+   start_date = Time.now.to_i
+   end_date = (Time.now + 4*24*60*60).to_i
+	hash = {"id"=>id, "budget_price"=>"120000", "cycle_delivery_price"=>"312", "ad_current_cost"=>"0.0", "ad_cost"=>"0.0", "delivery_time_limit"=>"96", "delivery_price_bc"=>"600", "delivery_price_youku"=>"0", "delivery_start_time"=>"#{start_date}", "delivery_end_time"=>"#{end_date}", "ad_group_budget_price"=>"120000", "ad_group_cost"=>"0.0", "account_buget_price"=>"200000", "account_cost"=>"0.0"}
+	rtb_key = "ad_#{id}_delivery_strategy"
 	puts rtb_key
 	RtbRedis.expense_node.mapped_hmset(rtb_key, hash)
-	RtbRedis.expense_node.sadd('budget_control',[15])
+	RtbRedis.expense_node.sadd('budget_control',[id])
    end
 end
 
